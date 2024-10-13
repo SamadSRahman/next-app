7099e0e1ffcd39319681b4c8884b9ecab2dc7949
Samad Rahman~ <samadsrahman@gmail.com>
2024-10-13 12:55:24 +0530

    deployment url added to cors

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -10,7 +10,7 @@ export async function OPTIONS(req: NextRequest) {
   return new NextResponse(null, {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:5173",
+      "Access-Control-Allow-Origin": "https://e-commerce-am.vercel.app/",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Credentials": "true",
